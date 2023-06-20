a5d743d8a353196fa1c7cb8f4ba5e8294780b51e
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2023-06-20 15:24:27 -0400

    update for 1.63, fixes
    fixed "" in filepath, fixed CET version hunting, reformatted for easier reading, updated for 1.63

--- FindAllErrors.bat
+++ FindAllErrors.bat
@@ -5,12 +5,25 @@ echo Establishing connection.....
 
 :: Check if we are in the cyberpunk directory or if the cyberpunk directory was dragged onto the script. Mostly stolen from Mana
 if "%~1" == "" (
-  pushd %~dp0
+  pushd "%~dp0"
   set "CYBERPUNKDIR=!CD!"
   popd
 ) else (
   set "CYBERPUNKDIR=%~1"
 )
+
+:: Check if the script folder is set to read-only
+pushd %~dp0
+if %errorlevel% neq 0 (
+    echo ERROR: The script folder is set to read-only.
+    echo Please ensure write permissions are available and try again.
+    popd
+    pause
+    exit /b
+)
+popd
+
+echo.
 
 ::if not, deploy R.A.B.I.D.S and shut down
 if not exist "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" (
@@ -44,7 +57,7 @@ for /f "tokens=2 delims==" %%a in ('wmic datafile where name^="!exe_path:\=\\!" 
 )      
 
 :: if not the current game version, yell at the user and deploy R.A.B.I.D.S.
-if not "!version!"=="3.0.71.13361" (
+if not "!version!"=="3.0.72.45053" (
   echo Please update the game before proceeding
   echo Deploying Roving Autonomous Bartmoss Interface Drones....
   FOR /L %%S IN (10, -1, 1) DO (
@@ -57,17 +70,33 @@ if not "!version!"=="3.0.71.13361" (
 :: Append version info to the output file
 echo Version: !version! > "%output_file%"
 
-:: get CET Version using wmic because nothing else wanted to work 
-set "version_dll_path=%CYBERPUNKDIR%\bin\x64\version.dll"
-for /f "tokens=2 delims==" %%c in ('wmic datafile where name^="!version_dll_path:\=\\!" get version /value') do (
-    for /f "delims=" %%d in ("%%c") do set "version_dll=%%d"
+:: get CET Version from the log file 
+set "cet_log=%CYBERPUNKDIR%\bin\x64\plugins\cyber_engine_tweaks\cyber_engine_tweaks.log"
+set "cet_version="
+set "cet_found="
+
+if exist "%cet_log%" (
+    for /f "delims=" %%a in ('findstr /I /C:"CET version" "%cet_log%"') do (
+        set "cet_version_line=%%a"
+    )
+    :: Trim the version from the line
+    for /f "tokens=8 delims= " %%a in ("!cet_version_line!") do (
+        set "cet_version=%%~a"
+        set "cet_found=1"
+    )
 )
-:: Append CET Version info to the output file
-echo Version: !version!  CET Version: !version_dll! > "%output_file%"
+
+:: if CET is not found, add CET to dll_not_found
+if not defined cet_found (
+    if defined dll_not_found (
+        set "dll_not_found=!dll_not_found!, CET"
+    ) else (
+        set "dll_not_found=CET"
+    )
+)
 
 :: Search for red4ext framework mod DLL files and check their versions
 set "dll_files=RED4ext.dll ArchiveXL.dll TweakXL.dll Codeware.dll"
-set "dll_not_found="
 
 for %%D in (%dll_files%) do (
     set "dll_version="
@@ -80,21 +109,33 @@ for %%D in (%dll_files%) do (
     )
     if not defined dll_found (
         if defined dll_not_found (
-            set "dll_not_found=!dll_not_found!, %%D"
+            set "dll_not_found=!dll_not_found!, %%~nD"
         ) else (
-            set "dll_not_found=%%D"
+            set "dll_not_found=%%~nD"
         )
     ) else (
-        echo %%D Version: !dll_version! >> "%output_file%"
+        echo %%~nD Version: !dll_version! >> "%output_file%"
     )
 )
 
-:: if any core mod dlls are not found, display an alert and write to the output file
+:: if any core mod dlls or CET are not found, display an alert and write to the output file
 if defined dll_not_found (
-    echo The following core mods are not installed: %dll_not_found% >> "%output_file%"
+    echo The following framework mods are not installed: %dll_not_found% >> "%output_file%"
 )
 
+:: Print CET version if it was found
+if defined cet_found (
+    echo CET Version: !cet_version! >> "%output_file%"
+)
+
+
 :: Parse through all files ending with .log, excluding those with .number.log pattern
+echo. >> "%output_file%" 
+echo ======================================================== >> "%output_file%"
+echo Succesfully Breached: %cyberpunkdir% >> "%output_file%"
+echo The following log files have errors: >> "%output_file%"
+echo ======================================================== >> "%output_file%"
+
 for /R "%CYBERPUNKDIR%" %%F in (*.log) do (
     set "filename=%%~nxF"
     setlocal enabledelayedexpansion
@@ -108,14 +149,29 @@ for /R "%CYBERPUNKDIR%" %%F in (*.log) do (
 
     REM Process non-excluded log files
     if "!exclude!"=="false" (
-        for /F "delims=" %%L in ('findstr /I "exception error failed" "%%F"') do (
-            echo Fault found in: %%~nxF "%%L" >> "%output_file%"
+        :: Initialize error flag to false
+        set "has_error=false"
+        
+        :: Check for any errors in the file. If found, set error flag to true
+        findstr /I "exception error failed" "%%F" >nul && set "has_error=true"
+        
+        :: If error is found, print filepath and process the error lines
+        if "!has_error!"=="true" (
+            echo. >> "%output_file%"
+            set "relative_path=%%~dpF"
+            set "relative_path=!relative_path:%CYBERPUNKDIR%=!"
+            echo !relative_path:~1!%%~nxF >> "%output_file%"
+            echo. >> "%output_file%"
+
+           for /F "delims=" %%L in ('findstr /I "exception error failed" "%%F"') do (
+                echo     %%L >> "%output_file%"
+                echo Daemons uploaded successfully, data found
+            )
         )
     )
-
     endlocal
-	echo Daemons uploaded successfully, data found
 )
+
 :: Open the LOGS folder in a new File Explorer window for easy navigation and uploading to Discord
 start "" "%CYBERPUNKDIR%\_LOGS"
 
