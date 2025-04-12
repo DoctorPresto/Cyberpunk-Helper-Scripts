e274ee80b4517f25e31f30c6566e714a7386f61d
manavortex <manavortex@gmail.com>
2025-04-12 15:52:05 +0200

    Update FindAllErrors.bat
    should improve visualc version detection. Please test before merging >.<

--- FindAllErrors.bat
+++ FindAllErrors.bat
@@ -8,16 +8,16 @@ for /L %%i in (1,1,60) do (
 )
 echo.
 
-:: Check if we are in the cyberpunk directory or if the cyberpunk directory was dragged onto the script. Mostly stolen from Mana
-if "%~1" == "" (
-  pushd "%~dp0"
+REM Check if we are in the cyberpunk directory or if the cyberpunk directory was dragged onto the script. Mostly stolen from Mana
+if [%~1] == [] (
+  pushd %~dp0
   set "CYBERPUNKDIR=!CD!"
   popd
 ) else (
   set "CYBERPUNKDIR=%~1"
 )
 
-:: Check if the script folder is set to read-only
+REM Check if the script folder is set to read-only
 pushd %~dp0
 if %errorlevel% neq 0 (
     echo ERROR: The script folder is set to read-only.
@@ -30,12 +30,12 @@ popd
 
 echo.
 
-::if not, deploy R.A.B.I.D.S and shut down
-if not exist "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" (
+REM if not, deploy R.A.B.I.D.S and shut down
+if not exist "!CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe" (
   echo.
-  echo Wake the f*ck up Samurai! This isn't your Cyberpunk install directory
-  echo Either place me in your Cyberpunk 2077 folder and try again
-  echo or drag and drop it onto me from Windows Explorer
+  echo Wake the f*ck up Samurai! This is not your Cyberpunk install directory!
+  echo Either place me in your Cyberpunk 2077 folder and try again,
+  echo or drag and drop it onto me from Windows Explorer.
   echo.
   echo Deploying Roving Autonomous Bartmoss Interface Drones....
   FOR /L %%S IN (10, -1, 1) DO (
@@ -45,15 +45,17 @@ if not exist "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" (
   goto :eof
 )
 
-:: ====================================================================
+REM ====================================================================
 
-:: visualc redistributable version
+REM visualc redistributable version
 set VC_VERSION=14.42.34433.0
 
-:: game version (of executable)
+REM game version (of executable)
 set LATESTVERSION=3.0.78.57301
 
-:: ====================================================================
+REM ====================================================================
+
+:START
 
 echo.
 echo Please select an option:
@@ -64,7 +66,7 @@ echo 2. Check for errors
 echo.
 
 set /p userOption=Enter your choice: 
-if "%userOption%"=="1" (
+if !userOption! equ 1 (
 	echo.
     echo Deleting all log files...
     for /R "%~dp0" %%G in (*.log *log.txt) do (
@@ -73,19 +75,19 @@ if "%userOption%"=="1" (
 	echo.
     echo All log files deleted successfully.
     echo Starting Cyberpunk 2077...
-    start "" "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" -modded
+    start "" "!CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe" -modded
     goto :eof
-) else if "%userOption%"=="2" (
+) else if !userOption! equ 2 (
     echo Deploying Datamine
 ) else (
     echo Invalid option, please enter 1 or 2.
-    goto START
+    goto :START
 )
 
-::if not, deploy R.A.B.I.D.S and shut down
-if not exist "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" (
+REM if not, deploy R.A.B.I.D.S and shut down
+if not exist "!CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe" (
   echo.
-  echo Wake the f*ck up Samurai! This isn't your Cyberpunk install directory
+  echo Wake the f*ck up Samurai! This is not your Cyberpunk install directory
   echo Either place me in your Cyberpunk 2077 folder and try again
   echo or drag and drop it onto me from Windows Explorer
   echo.
@@ -96,23 +98,23 @@ if not exist "%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe" (
   )
   goto :eof
 )
-:: Check if the LOGS folder already exists in the install directory, if not, create it
-if not exist "%CYBERPUNKDIR%\_LOGS" mkdir "%CYBERPUNKDIR%\_LOGS"
+REM Check if the LOGS folder already exists in the install directory, if not, create it
+if not exist "!CYBERPUNKDIR!\_LOGS" mkdir "!CYBERPUNKDIR!\_LOGS"
 
-:: Set the output file path for the FilteredLogs file
-set "output_file=%CYBERPUNKDIR%\_LOGS\FilteredLogs.txt"
+REM Set the output file path for the FilteredLogs file
+set "output_file=!CYBERPUNKDIR!\_LOGS\FilteredLogs.txt"
 
-:: if there's already a FilteredLogs.txt file, clear the content so that all of the errors listed were recorded on this run this run
-break > "%output_file%"
+REM if there's already a FilteredLogs.txt file, clear the content so that all of the errors listed were recorded on this run this run
+echo > "!output_file!"
 
-:: Set the path to the cyberpunk exe 
-set "exe_path=%CYBERPUNKDIR%\bin\x64\Cyberpunk2077.exe"
-:: Get exe version using PowerShell and capture the output
-for /f "usebackq delims=" %%i in (`powershell -Command "$file = Get-Item '%exe_path%'; $version = [string]$file.VersionInfo.FileMajorPart + '.' + [string]$file.VersionInfo.FileMinorPart + '.' + [string]$file.VersionInfo.FileBuildPart + '.' + [string]$file.VersionInfo.FilePrivatePart; Write-Output $version"`) do ( set "version=%%i" )
+REM Set the path to the cyberpunk exe 
+set "exe_path=!CYBERPUNKDIR!\bin\x64\Cyberpunk2077.exe"
+REM Get exe version using PowerShell and capture the output
+for /f "usebackq delims=" %%i in (`powershell -Command "$file = Get-Item '!exe_path!'; $version = [string]$file.VersionInfo.FileMajorPart + '.' + [string]$file.VersionInfo.FileMinorPart + '.' + [string]$file.VersionInfo.FileBuildPart + '.' + [string]$file.VersionInfo.FilePrivatePart; Write-Output $version"`) do ( set "version=%%i" )
 
-:: if not the current game version, yell at the user and deploy R.A.B.I.D.S.
-if not "!version!"=="%LATESTVERSION%" (
-  echo Please update the game before proceeding. The most recent game version is 2.21 with the executable version %LATESTVERSION%
+REM if not the current game version, yell at the user and deploy R.A.B.I.D.S.
+if not "!version!"=="!LATESTVERSION!" (
+  echo Please update the game before proceeding. The most recent game version is 2.21 with the executable version !LATESTVERSION!
   echo.
   echo Deploying Roving Autonomous Bartmoss Interface Drones....
   FOR /L %%S IN (10, -1, 1) DO (
@@ -123,26 +125,26 @@ if not "!version!"=="%LATESTVERSION%" (
   goto :eof
 )
 
-:: Append version info to the output file
-echo Game version: !version! > "%output_file%"
+REM Append version info to the output file
+echo Game version: !version! > "!output_file!"
 
-:: get CET Version from the log file 
-set "cet_log=%CYBERPUNKDIR%\bin\x64\plugins\cyber_engine_tweaks\cyber_engine_tweaks.log"
+REM get CET Version from the log file 
+set "cet_log=!CYBERPUNKDIR!\bin\x64\plugins\cyber_engine_tweaks\cyber_engine_tweaks.log"
 set "cet_version="
 set "cet_found="
 
-if exist "%cet_log%" (
-    for /f "delims=" %%a in ('findstr /I /C:"CET version" "%cet_log%"') do (
+if exist "!cet_log!" (
+    for /f "delims=" %%a in ('findstr /I /C:"CET version" "!cet_log!"') do (
         set "cet_version_line=%%a"
     )
-    :: Trim the version from the line
+    REM Trim the version from the line
     for /f "tokens=12 delims= " %%a in ("!cet_version_line!") do (
         set "cet_version=%%~a"
-        set "cet_found=1"
+        set cet_found=1
     )
 )
 
-:: if CET is not found, add CET to dll_not_found
+REM if CET is not found, add CET to dll_not_found
 if not defined cet_found (
     if defined dll_not_found (
         set "dll_not_found=!dll_not_found!, CET"
@@ -154,18 +156,18 @@ if not defined cet_found (
 set "redscript_files=engine\tools\scc.exe engine\tools\scc_lib.dll engine\config\base\scripts.ini r6\config\cybercmd\scc.toml"
 set "redscript_missing="
 
-:: Check for the existence of each Redscript mod file
-for %%R in (%redscript_files%) do (
-    if not exist "%CYBERPUNKDIR%\%%R" (
+REM Check for the existence of each Redscript mod file
+for %%R in (!redscript_files!) do (
+    if not exist "!CYBERPUNKDIR!\%%R" (
         if defined redscript_missing (
-            set "redscript_missing=!redscript_missing!, %%R"
+            set "redscript_missing=!redscript_files!, %%R"
         ) else (
             set "redscript_missing=%%R"
         )
     )
 )
 
-:: If any Redscript mod files are missing, add Redscript to the list of missing frameworks
+REM If any Redscript mod files are missing, add Redscript to the list of missing frameworks
 if defined redscript_missing (
     if defined dll_not_found (
         set "dll_not_found=!dll_not_found!, Redscript"
@@ -173,10 +175,11 @@ if defined redscript_missing (
         set "dll_not_found=Redscript"
     )
 ) 
-:: Search for red4ext framework mod DLL files and check their versions
+
+REM Search for red4ext framework mod DLL files and check their versions
 set "dll_files=RED4ext.dll ArchiveXL.dll TweakXL.dll Codeware.dll"
 
-for %%D in (%dll_files%) do (
+for %%D in (!dll_files!) do (
     set "dll_version="
     set "dll_found="
     for /R "%CYBERPUNKDIR%\red4ext" %%F in ("*%%D") do (
@@ -196,7 +199,7 @@ for %%D in (%dll_files%) do (
     )
 )
 
-:: check for RHT
+REM check for RHT
 if exist "%CYBERPUNKDIR%\red4ext\plugins\RedHotTools\RedHotTools.dll" (
     for /f "delims=" %%a in ('powershell -Command "$versionInfo = Get-Command '%CYBERPUNKDIR%\red4ext\plugins\RedHotTools\RedHotTools.dll' | ForEach-Object { $_.FileVersionInfo.ProductVersion }; if ($versionInfo) { Write-Output $versionInfo }"') do (
         set "dll_version=%%a"
@@ -207,71 +210,60 @@ if exist "%CYBERPUNKDIR%\red4ext\plugins\RedHotTools\RedHotTools.dll" (
     )
 )
 
-:: if any core mod dlls or CET are not found, display an alert and write to the output file
+REM if any core mod dlls or CET are not found, display an alert and write to the output file
 if defined dll_not_found (
-    echo The following framework mods are not detected: %dll_not_found% >> "%output_file%"
+    echo The following framework mods are not detected: !dll_not_found! >> "%output_file%"
 )
 
-:: Print CET version if it was found
+REM Print CET version if it was found
 if defined cet_found (
     echo CET Version: !cet_version! >> "%output_file%"
 )
 
 if not defined redscript_missing (
-	echo
-    echo Redscript: installed correctly >> "%output_file%"
+  echo. >> "%output_file%"
+  echo Redscript: installed correctly >> "%output_file%"
 )
 
-:: check for visualc redistributable
-reg query HKEY_CLASSES_ROOT\Installer\Dependencies\VC,redist.x64,amd64,14.42,bundle /V Version | Find "%VC_VERSION%" >nul
-if %errorlevel% equ 0 (
-  echo Visual C++ x64 Redist %VC_VERSION% installed >> "%output_file%"
-) else (
-  echo. >> "%output_file%"
-  echo Visual C++ x64 Redist %VC_VERSION% not installed >> "%output_file%"
-  echo Please download and install the 64-bit version from Microsoft: >> "%output_file%"
-  echo https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170#visual-studio-2015-2017-2019-and-2022 >> "%output_file%"
-  echo. >> "%output_file%"
-)
+call :CheckVCVersion "!VC_VERSION!"
 
-
-:: Parse through all files ending with .log, excluding those with .number.log pattern
+REM Parse through all files ending with .log, excluding those with .number.log pattern
 echo. >> "%output_file%" 
 echo ======================================================== >> "%output_file%"
-echo Successfully Breached: %cyberpunkdir% >> "%output_file%"
+echo Successfully Breached: !CYBERPUNKDIR! >> "%output_file%"
 echo The following log files have errors: >> "%output_file%"
 echo ======================================================== >> "%output_file%"
 
-for /R "%CYBERPUNKDIR%" %%F in (*.log *log.txt) do (
+for /R "!CYBERPUNKDIR!" %%F in (*.log *log.txt) do (
     set "filename=%%~nxF"
     setlocal enabledelayedexpansion
-    set "exclude=false"
+    set exclude=false
     
-::  Check if the file name contains two dots or matches the date pattern
+REM  Check if the file name contains two dots or matches the date pattern
     echo "!filename!" | findstr /R /C:".*\..*\.." >nul
     if !errorlevel! equ 0 (
-        set "exclude=true"
+        set exclude=true
     ) else (
         echo "!filename!" | findstr /r /c:".*20[0-9][0-9]-[0-1][0-9]-[0-3][0-9].*" >nul
     )
     if !errorlevel! equ 0 (
-        set "exclude=true"
+        set exclude=true
     )
 
-::  Process non-excluded log files
+REM  Process non-excluded log files
     if "!exclude!"=="false" (
-        :: Initialize error flag to false
-        set "has_error=false"
-        :: Check for any errors in the file. If found, set error flag to true
+        REM Initialize error flag to false
+        set has_error=false
+        REM Check for any errors in the file. If found, set error flag to true
         for /F "delims=" %%L in ('findstr /I "exception error failed" "%%F" ^| findstr /V /I /C:"Failed to create record" ^| findstr /V /I /C:"[AMM Error] Non-localized string found" ^| findstr /V /I /C:"reason: Record already exists" ^| findstr /V /I /C:"[Info]"') do (
-            set "has_error=true"
+            set has_error=true
         )
         
-        :: If error is found, print filepath and process the error lines
+        REM If error is found, print filepath and process the error lines
         if "!has_error!"=="true" (
             echo. >> "%output_file%"
             set "relative_path=%%~dpF"
-            set "relative_path=!relative_path:%CYBERPUNKDIR%=!"
+            set "relative_path=!relative_path:!CYBERPUNKDIR!=!"
             echo !relative_path:~1!%%~nxF >> "%output_file%"
             echo. >> "%output_file%"
 
@@ -284,8 +276,83 @@ for /R "%CYBERPUNKDIR%" %%F in (*.log *log.txt) do (
     endlocal
 )
 
-:: Open the LOGS folder in a new File Explorer window for easy navigation and uploading to Discord
+
+
+REM Open the LOGS folder in a new File Explorer window for easy navigation and uploading to Discord
 start "" "%CYBERPUNKDIR%\_LOGS"
 start "" "%CYBERPUNKDIR%\_LOGS\FilteredLogs.txt"
 
 endlocal
+
+goto :eof
+
+
+
+:CheckVCVersion
+REM Checks if the installed VC++ Redistributable version is >= <MinVersion>.
+REM Returns 0 if OK, 1 if too old or not found.
+    setlocal
+    set "MIN_VERSION=%~1"
+    
+    REM Parse the minimum version into components
+    for /f "tokens=1-4 delims=." %%A in ("%MIN_VERSION%") do (
+        set MIN_MAJOR=%%A
+        set MIN_MINOR=%%B
+        set MIN_BUILD=%%C
+        set MIN_REVISION=%%D
+    )
+
+    REM Query the registry for the installed version
+    set "REG_KEY=HKEY_CLASSES_ROOT\Installer\Dependencies\VC,redist.x64,amd64,14.42,bundle"
+    for /f "tokens=2,*" %%A in (
+        'reg query "%REG_KEY%" /v Version 2^>nul ^| find "Version"'
+    ) do (
+        set "INST_VERSION=%%B"
+    )
+
+    if not defined INST_VERSION (
+        echo. >> "%output_file%"
+        echo Visual C++ x64 Redist %VC_VERSION% not installed >> "%output_file%"
+        echo Please download and install the 64-bit version from Microsoft: >> "%output_file%"
+        echo https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170#visual-studio-2015-2017-2019-and-2022 >> "%output_file%"
+        echo. >> "%output_file%"
+        endlocal
+        goto :eof
+    )
+
+    REM Parse installed version into components
+    for /f "tokens=1-4 delims=." %%A in ("%INST_VERSION%") do (
+        set INST_MAJOR=%%A
+        set INST_MINOR=%%B
+        set INST_BUILD=%%C
+        set INST_REVISION=%%D
+    )
+
+    REM Compare version components numerically
+    if %INST_MAJOR% gtr %MIN_MAJOR% (
+        set VERSION_OK=1
+    ) else if %INST_MAJOR% equ %MIN_MAJOR% (
+        if %INST_MINOR% gtr %MIN_MINOR% (
+            set VERSION_OK=1
+        ) else if %INST_MINOR% equ %MIN_MINOR% (
+            if %INST_BUILD% gtr %MIN_BUILD% (
+                set VERSION_OK=1
+            ) else if %INST_BUILD% equ %MIN_BUILD% (
+                if %INST_REVISION% geq %MIN_REVISION% (
+                    set VERSION_OK=1
+                )
+            )
+        )
+    )
+
+    if defined VERSION_OK (
+        echo Visual C++ x64 Redist %VC_VERSION% installed ✔ Required: %MIN_VERSION% >> "%output_file%"        
+    ) else (    
+        echo. >> "%output_file%"
+        echo Visual C++ x64 Redist %VC_VERSION% installed ✖ Required: %MIN_VERSION% >> "%output_file%"
+        echo Please download and install the 64-bit version from Microsoft: >> "%output_file%"
+        echo https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170#visual-studio-2015-2017-2019-and-2022 >> "%output_file%"
+        echo. >> "%output_file%"        
+    )
+    endlocal
+    goto :eof
