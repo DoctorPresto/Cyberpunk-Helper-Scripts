6e46881a978aaf36dbad60e9ba4ebd291eeef317
manavortex <manavortex@gmail.com>
2025-07-18 18:30:17 +0200

    Update FindAllErrors.bat
    bump exe version for 2.3

--- FindAllErrors.bat
+++ FindAllErrors.bat
@@ -51,7 +51,7 @@ REM visualc redistributable version
 set VC_VERSION=14.42.34433.0
 
 REM game version (of executable)
-set LATESTVERSION=3.0.78.57301
+set LATESTVERSION=3.0.80.33925
 
 REM ====================================================================
 
